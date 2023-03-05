d80da43a2b99350aec4abb3a76f077e07a3d0205
Andrey Abakumov <andrey9864rus@mail.ru>
2023-03-05 12:05:31 +0400

    Initial Commit

---
+++ FathersKick2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FathersKick2
+//
+//  Created by Андрей Абакумов on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
